659295dc46f5931e4b380fa940df683f69c3c0c7
Guilherme <jg21504956@gmail.com>
2025-08-16 09:05:05 -0300

    Adicionando o componente IconButton

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -3,17 +3,23 @@ import { Link } from "react-router";
 
 //components
 import InputSearch from "../components/inputs/InputSearch";
+import IconButton from "../components/buttons/IconButton";
 
 
 //navbar
 const NavBar = ()=>(
-    <nav>
+    <nav className="w-full h-[10vh] flex items-center justify-between">
         <Link to='/'>
             Blog
         </Link>
-        <InputSearch/>
+        <InputSearch input_placeholder="Pesquisar Post ou Usuario"/>
         <ul>
-            
+            <li>
+                <IconButton icon='/src/assets/icons/write.svg' button_content='write' link='' />
+            </li>
+            <li>
+
+            </li>
         </ul>
     </nav>
 )
